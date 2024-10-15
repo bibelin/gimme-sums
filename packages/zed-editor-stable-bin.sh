184d715a37da642668fcb77c8f9cab7317989221
Balian of Ibelin <balian1belin@yandex.ru>
2024-10-15 10:34:30 +0300

    Create zed-editor-stable-bin.sh

---
+++ packages/zed-editor-stable-bin.sh
@@ -0,0 +1,7 @@
+pkgver="0.156.2"
+url="https://zed.dev"
+_channel="stable"
+links=(
+  "${url}/api/releases/${_channel}/${pkgver}/zed-linux-x86_64.tar.gz"
+  "${url}/api/releases/${_channel}/${pkgver}/zed-linux-aarch64.tar.gz"
+)
