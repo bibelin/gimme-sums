1e9dec75e6483e3b2fe76246ab58b73c2b7ede58
Balian of Ibelin <balian1belin@yandex.ru>
2024-10-15 21:54:52 +0300

    Create ollama-bin.sh

---
+++ packages/ollama-bin.sh
@@ -0,0 +1,6 @@
+pkgver="0.3.13"
+gives="ollama"
+links=(
+  "https://github.com/${gives}/${gives}/releases/download/v${pkgver}/${gives}-linux-amd64.tgz"
+  "https://github.com/${gives}/${gives}/releases/download/v${pkgver}/${gives}-linux-arm64.tgz"
+)
