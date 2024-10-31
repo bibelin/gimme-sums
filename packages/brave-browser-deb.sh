63fbab00e8b27a35949d9843fe936190b0982ca9
Balian of Ibelin <balian1belin@yandex.ru>
2024-10-31 13:43:14 +0300

    upd(brave-browser-deb): `1.71.118` -> `1.71.121`

--- packages/brave-browser-deb.sh
+++ packages/brave-browser-deb.sh
@@ -1,5 +1,5 @@
 gives="brave-browser"
-pkgver="1.71.118"
+pkgver="1.71.121"
 links=(
   "https://brave-browser-apt-release.s3.brave.com/pool/main/b/${gives}/${gives}_${pkgver}_amd64.deb"
   "https://brave-browser-apt-release.s3.brave.com/pool/main/b/${gives}/${gives}_${pkgver}_arm64.deb"
